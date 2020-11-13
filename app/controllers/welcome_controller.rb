0e522551550508d3547cfad003b4d9f477b8f2f9
PatriciaArnedo <paaarnedo@gmail.com>
2020-11-12 19:25:51 -0500

    added featured artists to home page, fixed cart spacing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
 
 
     def home
+        @artwork1 = Artwork.find(62)
+        @artwork2 = Artwork.find(39)
+        @artwork3 = Artwork.find(53)
         
     end
 end
